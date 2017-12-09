7eaa70ccc240a2dee13101a895eb42c0fbd12069
mocuishla <llanesagudelo@gmail.com>
2017-12-09 19:29:34 +0100

    He creado Censored

---
+++ CENSORED/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CENSORED
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List <string> palabrotas = new List<string>();
+
+            palabrotas.Add("Mierda");
+            palabrotas.Add("Cabron");
+            palabrotas.Add("Puta");
+
+            Console.WriteLine(palabrotas.Count);
+
+
+
+
+        }
+
+    }
+}
